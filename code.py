9c03f81cfdf0da8485ec9345c34364de8a75167a
Jvaneyll <jonathanvaneyll@yahoo.fr>
2018-07-08 20:02:53 +0200

    Working version of date/time import from RTC via i2c connection "code.py". Includes a script to scan all i2c devices connected to it and check connection stability.

--- code.py
+++ code.py
@@ -4,33 +4,29 @@ import time
 import busio
 import adafruit_pcf8523
 
-#Scan for I2C device 
-i2c = busio.I2C(board.SCL, board.SDA)
- 
-while not i2c.try_lock():
-    pass
- 
- 
-while True:
-    adr= i2c.scan()
-    print("I2C addresses found:", [hex(device_address)
-                                   for device_address in i2c.scan()])
-    #if adr
-    #    rtc = adafruit_pcf8523.PCF8523(i2c)
-    time.sleep(0.1)
+adr=[]
+## initialize i2c I/O of RTC
+i2c = busio.I2C(board.SCL, board.SDA,frequency=400000)
+rtc = adafruit_pcf8523.PCF8523(i2c)
+if 'i2c' in locals() and 'rtc' in locals():
+    t = rtc.datetime
+    #print(t)
+else:
+    print("i2c bus and RTC not initialized !")
+    quit()
 
-## initialize I/O
+print(t)
+print(t.tm_year)
+print(t.tm_mon)
+print(t.tm_mday)
+print(t.tm_hour)
+print(t.tm_min)
+print(t.tm_sec)
+print(t.tm_wday)
 
-#I2C for RTC clock
-#i2c_bus = busio.I2C(board.SCL, board.SDA)
-#print(i2c_bus)
 
 
 #Led for output_signal
 #led = digitalio.DigitalInOut(board.D13)
 #led.direction = digitalio.Direction.OUTPUT
 
-##initialize variables
-#t = rtc.datetime
-#print(t)
-#print(t.tm_date,t.tm_hour, t.tm_min)
